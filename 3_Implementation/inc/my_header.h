fcbd08eebf607fd36ca4cddf09c96ec9fd0d7c6d
Prabhat Roshan <prabhat9546784170@gmail.com>
2021-04-14 19:03:54 +0530

    Update my_header.h

--- 3_Implementation/inc/my_header.h
+++ 3_Implementation/inc/my_header.h
@@ -1,15 +1,3 @@
-/**
- * @file my_header.h
- * @author  Prabhat Roshan
- * @brief functions for different mathematical operations
- * @version 0.1
- * @date 2021-04-13
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
-
 
 #ifndef _myheaderh__
 #define _myheaderh__
