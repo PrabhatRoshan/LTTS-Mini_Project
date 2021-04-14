77ab7c2b102d39a64d1fea5c4027de62c53fe396
Prabhat Roshan <prabhat9546784170@gmail.com>
2021-04-14 15:58:38 +0530

    Add files via upload

---
+++ 3_Implementation/inc/my_header.h
@@ -0,0 +1,34 @@
+/**
+ * @file my_header.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+
+#ifndef _myheaderh__
+#define _myheaderh__
+#include<stdio.h>
+
+
+// Function prototype declaration
+/**
+ * @brief 
+ * 
+ */
+void calculator_operations();
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+
+
+#endif
