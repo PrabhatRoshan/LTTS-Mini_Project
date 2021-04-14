71db8462eac2aa97ea0db69b1b5c1dc7024fff00
Prabhat Roshan <prabhat9546784170@gmail.com>
2021-04-14 16:17:30 +0530

    Add files via upload

---
+++ 3_Implementation/inc/my_header.h
@@ -0,0 +1,29 @@
+/**
+ * @file my_header.h
+ * @author  Prabhat Roshan
+ * @brief functions for different mathematical operations
+ * @version 0.1
+ * @date 2021-04-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+
+#ifndef _myheaderh__
+#define _myheaderh__
+#include<stdio.h>
+
+
+void calculator_operations();
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+
+
+#endif
